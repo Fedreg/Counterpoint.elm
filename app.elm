bb45fd4b11cfc2ab67a0a29355071ab0a2c905d4
Fed Reggiardo <bksgtr@gmail.com>
2016-11-06 18:48:27 -0500

    added octave and duration selection functions.  Lots of commented out code in this

--- app.elm
+++ app.elm
@@ -5,7 +5,7 @@ import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (..)
-import Debug exposing (log)
+import Array exposing (..)
 import Regex exposing (..)
 
 
@@ -56,11 +56,81 @@ parseNotes string =
         |> List.map .match
 
 
-finalArr =
-    List.map (String.toList >> List.map String.fromChar)
+sustain duration =
+    case duration of
+        "w" ->
+            4.0
+
+        "h" ->
+            2.0
+
+        "q" ->
+            1.0
+
+        "e" ->
+            0.5
+
+        "s" ->
+            0.25
+
+        _ ->
+            0.0
+
+
+octave num =
+    case num of
+        1 ->
+            1
+
+        _ ->
+            2 * (num - 1)
 
 
 
+{--
+finalArr item =
+    if List.length item == 3 then
+
+        List.map (first ++ third  ++ fourth ++ ) item
+
+    else
+        List.map (firstTwo  ++ third  ++ fourth ++ ) item
+
+
+first a =
+    List.map (String.left 1) a
+
+
+firstTwo a =
+    List.map (String.left 2) a
+
+
+third a =
+    List.map (String.slice 2 3) a
+
+
+fourth a =
+    List.map (String.right 1) a
+
+
+
+        List.map (String.toList >> List.map String.fromChar)
+frequencies =
+    { c = 130.81
+    , c# = 139.00
+    , d = 146.83
+    , d# = 156.00
+    , e = 164.81
+    , f = 174.61
+    , f# = 185.00
+    , g = 196.00
+    , g# = 208.00
+    , a = 220.00
+    , a# = 233.00
+    , b = 246.94
+    , r = 0.0
+ }
+-}
 -- VIEW
 
 
@@ -74,4 +144,6 @@ view model =
             ]
             []
         , button [ onClick PrepareNotes ] [ text "Play Notes" ]
+        , div [] [ text (toString (parseNotes model.notesToBeParsed)) ]
+          --, div [] [ text (toString (finalArr parseNotes)) ]
         ]
